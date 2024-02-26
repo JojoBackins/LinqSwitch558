bbb05f35d4f62979f5b70549e91fccd57110220c
jojogreen <jojogreen1408@gmail.com>
2024-02-26 17:14:23 +0300

    Добавьте файлы проекта.

---
+++ Suit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LinqSwitch558;
+
+namespace LinqSwitch558
+{
+    
+    
+        public enum Suits
+        {
+            Diamonds,
+            Clubs,
+            Hearts,
+            Spades,
+        }
+    
+}
